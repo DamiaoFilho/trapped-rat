6153025a9299792dc0ea194689030abd5a4a4b26
DamiaoFilho <damiaofilho0123@gmail.com>
2023-11-07 10:16:28 -0300

    first commit

---
+++ classes.py
@@ -0,0 +1,8 @@
+class Cell:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True}
+        self.visited = False
+
+    def draw(self):
+        x, y = self.x * TILE, self.y * TILE
